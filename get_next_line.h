aba8f1b15f5c4333bcb7bdf8197e4eb0262fd6fc
Aitor Garcia ballesteros <aigarcia@cbr9s2.42barcelona.com>
2022-09-19 13:55:52 +0200

    gnl 2

--- get_next_line.h
+++ get_next_line.h
@@ -5,19 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aigarcia <aigarcia@student.42barc...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/31 09:59:10 by aigarcia          #+#    #+#             */
-/*   Updated: 2022/08/31 09:59:11 by aigarcia         ###   ########.fr       */
+/*   Created: 2022/09/05 14:16:59 by aigarcia          #+#    #+#             */
+/*   Updated: 2022/09/05 14:17:00 by aigarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1000
-# endif
 
 char	*get_next_line(int fd);
 char	*ft_read_left_str(int fd, char *left_str);
